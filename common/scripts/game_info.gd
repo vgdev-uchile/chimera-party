0245d9cbf807d71e6463148b5075238b1994be3b
Elías Zelada <elixs.777@gmail.com>
2024-05-08 01:56:42 -0400

    deleting common

--- common/scripts/game_info.gd
+++
@@ -1,7 +0,0 @@
-class_name GameInfo
-extends Resource
-
-@export var name: String
-@export_multiline var description: String
-@export var image: Texture2D
-@export var inputs: Array[GameInput]
--- common/scripts/game_input.gd
+++
@@ -1,6 +0,0 @@
-class_name GameInput
-extends Resource
-
-
-@export_flags("Left", "Right", "Up", "Down", "A", "B") var inputs: int
-@export var description: String
